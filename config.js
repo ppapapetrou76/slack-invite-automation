0164462585711344d637a3b917a22d4e97975ad8
Patroklos Papapetrou <ppapapetrou76@gmail.com>
2018-02-16 15:26:10 +0200

    Migrate to Voxxed Greece

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   // your community or team name to display on join page.
-  community: process.env.COMMUNITY_NAME || 'Voxxed Athens',
+  community: process.env.COMMUNITY_NAME || 'Voxxed Greece',
   // your slack team url (ex: socketio.slack.com)
-  slackUrl: process.env.SLACK_URL || 'voxxedathens.slack.com',
+  slackUrl: process.env.SLACK_URL || 'voxxed-greece.slack.com',
   // access token of slack
   // You can generate it in https://api.slack.com/web#auth
   // You should generate the token in admin user, not owner.
@@ -12,7 +12,7 @@ module.exports = {
   //   curl -X POST 'https://YOUR-SLACK-TEAM.slack.com/api/users.admin.invite' \
   //   --data 'email=EMAIL&token=TOKEN&set_active=true' \
   //   --compressed
-  slacktoken: process.env.SLACK_TOKEN || 'xoxp-139780327954-176693675474-176625260627-a5b891c3a236ab91e0351f3a32f88fca',
+  slacktoken: process.env.SLACK_TOKEN || 'xoxp-86278976164-86232138035-315732351937-b68e4e3b2ac898ce342b3d45d0e8d6b3',
   // an optional security measure - if it is set, then that token will be required to get invited.
   inviteToken: process.env.INVITE_TOKEN || null,
 
